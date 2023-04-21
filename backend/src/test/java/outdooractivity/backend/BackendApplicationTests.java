589cd9ff0476c50a5f84db1afc3560d9bdde68c0
teresakoetter <t.koetter@mailbox.org>
2023-04-21 12:43:37 +0200

    backend initialized

---
+++ backend/src/test/java/outdooractivity/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package outdooractivity.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
