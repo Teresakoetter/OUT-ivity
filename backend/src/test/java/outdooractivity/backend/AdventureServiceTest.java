8597dd7b0e939c3d3b799796b6b5ad5bdd14647e
teresakoetter <t.koetter@mailbox.org>
2023-04-23 19:11:07 +0200

    getAll JUnit tested

---
+++ backend/src/test/java/outdooractivity/backend/AdventureServiceTest.java
@@ -0,0 +1,35 @@
+package outdooractivity.backend;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+@AutoConfigureMockMvc
+@SpringBootTest
+public class AdventureServiceTest {
+    @Mock
+    private AdventureRepositoryInterface adventureRepositoryInterfaceMock;
+    private AdventureService adventureService;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        adventureService = new AdventureService(adventureRepositoryInterfaceMock);
+    }
+
+    @Test
+    public void getAll_shouldInvoke_findAll(){
+        //GIVEN
+        //WHEN
+        adventureService.getAll();
+        //THEN
+        verify(adventureRepositoryInterfaceMock).findAll();
+
+    }
+}
+
