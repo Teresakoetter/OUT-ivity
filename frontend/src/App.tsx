87d06499d213efe4bfcd958a74ed828934cd919b
teresakoetter <t.koetter@mailbox.org>
2023-04-25 16:48:07 +0200

    basic react routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,25 @@ import UseAdventure from "./UseAdventure";
 import AdventureGallery from "./AdventureGallery";
 import AddAdventure from "./AddAdventure";
 import AdventureDetailCard from "./AdventureDetailCard";
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 
 function App() {
     const {adventures, addAdventure, deleteAdventure} = UseAdventure()
     return (
-        <div className="App">
-            <p>OUT-ivity</p>
-            {<AdventureGallery adventures={adventures}/>}
-            {<AddAdventure addAdventure={addAdventure}/>}
-            {<AdventureDetailCard deleteAdventure={deleteAdventure}/>}
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <p>OUT-ivity</p>
+                <Routes>
+                    <Route element={<Navigate to="/adventures"/>}/>
+                    <Route path="/adventures"
+                           element={<AdventureGallery adventures={adventures}/>}/>
+                    <Route path="/adventures/add"
+                           element={<AddAdventure addAdventure={addAdventure}/>}/>
+                    <Route path="/adventures/:id"
+                           element={<AdventureDetailCard deleteAdventure={deleteAdventure}/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
