0fdc18f843c81dedfd6a9327a92fd3f214b89d51
teresakoetter <t.koetter@mailbox.org>
2023-04-26 12:54:47 +0200

    wooden design added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,21 +7,24 @@ import AdventureDetailCard from "./AdventureDetailCard";
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import Header from "./Header";
 
+
 function App() {
     const {adventures, addAdventure, deleteAdventure} = UseAdventure()
     return (
         <BrowserRouter>
             <div className="App">
                 <Header/>
-                <Routes>
-                    <Route element={<Navigate to="/adventures"/>}/>
-                    <Route path="/adventures"
-                           element={<AdventureGallery adventures={adventures}/>}/>
-                    <Route path="/adventures/add"
-                           element={<AddAdventure addAdventure={addAdventure}/>}/>
-                    <Route path="/adventures/:id"
-                           element={<AdventureDetailCard deleteAdventure={deleteAdventure}/>}/>
-                </Routes>
+                <div className="container">
+                    <Routes>
+                        <Route element={<Navigate to="/adventures"/>}/>
+                        <Route path="/adventures"
+                               element={<AdventureGallery adventures={adventures}/>}/>
+                        <Route path="/adventures/add"
+                               element={<AddAdventure addAdventure={addAdventure}/>}/>
+                        <Route path="/adventures/:id"
+                               element={<AdventureDetailCard deleteAdventure={deleteAdventure}/>}/>
+                    </Routes>
+                </div>
             </div>
         </BrowserRouter>
     );
