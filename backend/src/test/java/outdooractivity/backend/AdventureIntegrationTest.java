991378602addb9540c281a997f83c76b5fa36884
teresakoetter <t.koetter@mailbox.org>
2023-04-28 13:50:17 +0200

    addition to update Integration test: test rewritten

--- backend/src/test/java/outdooractivity/backend/AdventureIntegrationTest.java
+++ backend/src/test/java/outdooractivity/backend/AdventureIntegrationTest.java
@@ -140,7 +140,55 @@ class AdventureIntegrationTest {
                         []
                         """));
     }
-}
 
+    @Test
+    @DirtiesContext
+    void updateAdventure_shouldReturnUpdatedAdventure() throws Exception {
+        mockMvc.perform(post("/api/adventures")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "id": "1",
+                                "quote": "quote1",
+                                "name": "name1",
+                                "description": "description1"
+                                }
+                                """
+                        ))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                {
+                                "id": "1",
+                                "quote": "quote1",
+                                "name": "name1",
+                                "description": "description1"
+                                }
+                                """
+                ));
+        mockMvc.perform(put("/api/adventures/1/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "id": "1",
+                                "quote": "quote1",
+                                "name": "name1",
+                                "description": "description1"
+                                }
+                                """
+                        ))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                {
+                                "id": "1",
+                                "quote": "quote1",
+                                "name": "name1",
+                                "description": "description1"
+                                }
+                                """
+                ));
+    }
+}
 
 
--- frontend/src/AdventureDetailCard.tsx
+++ frontend/src/AdventureDetailCard.tsx
@@ -1,16 +1,22 @@
 import './AdventureCard.css'
 import UseAdventureDetail from "./UseAdventureDetail";
+import {Adventure} from "./Adventure";
+import {useNavigate} from "react-router-dom";
 
 type AdventureDetailCardProps = {
     deleteAdventure: (id: string) => void
+    updateAdventure: (adventure: Adventure) => void
 }
 
 export default function AdventureDetailCard(props: AdventureDetailCardProps) {
+
     const {adventure} = UseAdventureDetail()
+    const navigate = useNavigate();
 
     function onDeleteClick() {
         if (adventure) {
             props.deleteAdventure(adventure.id)
+            navigate("/adventures")
         }
 
     }
@@ -30,6 +36,10 @@ export default function AdventureDetailCard(props: AdventureDetailCardProps) {
                         {adventure.description}
                         <br/>
                         <button onClick={onDeleteClick}>delete adventure</button>
+                        <button onClick={() => {
+                            navigate("/adventures/update/" + adventure.id)
+                        }}>update adventure
+                        </button>
                     </div>
                     : <div>Loading...</div>
             }
