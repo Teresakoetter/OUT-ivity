137d1f5d05caad007c856dae290d0e8269832105
teresakoetter <t.koetter@mailbox.org>
2023-05-05 13:22:09 +0200

    IntegrationTest DELETE working

--- backend/src/test/java/outdooractivity/backend/AdventureIntegrationTest.java
+++ backend/src/test/java/outdooractivity/backend/AdventureIntegrationTest.java
@@ -115,14 +115,14 @@ class AdventureIntegrationTest {
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
-                                {
-                                "id": "1",
-                                "quote": "quote1",
-                                "name": "name1",
-                                "description": "description1"
-                                }
-                                """
-                        ))
+                        {
+                        "id": "1",
+                        "quote": "quote1",
+                        "name": "name1",
+                        "description": "description1"
+                        }
+                        """
+                ))
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
@@ -154,19 +154,35 @@ class AdventureIntegrationTest {
     @DirtiesContext
     @Test
     void deleteAdventure_shouldDeleteAdventureWithCorrespondingId() throws Exception {
-        String saveResult = mockMvc.perform(
-                        post("http://localhost:8080/api/adventures")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("""
-                                                {
-                                        "id": "1",
-                                        "quote": "quote1",
-                                        "name": "name1",
-                                        "description": "description1"
-                                        }
-                                                """)
+        MockMultipartFile file = new MockMultipartFile("file", "filename.png", MediaType.IMAGE_JPEG_VALUE, "test data".getBytes());
+        MockMultipartFile data = new MockMultipartFile("data", null, MediaType.APPLICATION_JSON_VALUE, """
+                {
+                "id": "1",
+                "quote": "quote1",
+                "name": "name1",
+                "description": "description1"
+                }
+                """.getBytes());
+        File fileToUpload = File.createTempFile("image", null);
+        file.transferTo(fileToUpload);
 
+        when(cloudinary.uploader()).thenReturn(uploader);
+        when(uploader.upload(any(), any())).thenReturn(Map.of("url", "test-url"));
+
+        String saveResult = mockMvc.perform(multipart("/api/adventures")
+                        .file(data)
+                        .file(file)
                 )
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                        "id": "1",
+                        "quote": "quote1",
+                        "name": "name1",
+                        "description": "description1"
+                        }
+                        """
+                ))
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
@@ -231,92 +247,6 @@ class AdventureIntegrationTest {
                                 """
                 ));
     }
-
-    @Test
-    @DirtiesContext
-    void addAdventure_shouldReturnAddedAdventure1() throws Exception {
-        // create a mock file to upload
-        MockMultipartFile file = new MockMultipartFile("file", "test-image.jpg", "image/jpeg", "test-image-data".getBytes());
-
-        // create a JSON object representing the Adventure to add
-        JSONObject data = new JSONObject();
-        data.put("id", "1");
-        data.put("name", "Test Adventure");
-        data.put("quote", "This is a test quote.");
-        data.put("description", "This is a test description.");
-        // create a JSON object representing the request body
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("data", data);
-
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/adventures")
-                        .file(file)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody.toString()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.id").value("1"))
-                .andExpect(jsonPath("$.data.name").value("Test Adventure"))
-                .andExpect(jsonPath("$.data.quote").value("This is a test quote."))
-                .andExpect(jsonPath("$.data.description").value("This is a test description."));
-    }
-
-    @Autowired
-    AdventureService adventureService;
-
-    @Test
-    @DirtiesContext
-    void addAdventure_shouldReturnAddedAdventure3() throws Exception {
-        Adventure adventure = new Adventure("1", "name1", "quote1", "description1", "url1");
-        MockMultipartFile image = new MockMultipartFile("file", "filename.txt", "text/plain", "test data".getBytes());
-
-
-        given(adventureService.addAdventure(any(Adventure.class), any(MultipartFile.class))).willReturn(adventure);
-
-        mockMvc.perform(multipart("/api/adventures")
-                        .file(image)
-                        .param("data", new ObjectMapper().writeValueAsString(adventure))
-                        .contentType(MediaType.MULTIPART_FORM_DATA_VALUE))
-                .andExpect(status().isOk());
-
-    }
-
-    @Test
-    void addAdventure_shouldReturnAddedAdventure4() throws Exception {
-        byte[] imageBytes = Files.readAllBytes(Paths.get("http://res.cloudinary.com/dg3cegpdq/image/upload/v1683125076/pcvnrze0dyjxoqilanjr.jpg"));
-
-        MockMultipartFile file = new MockMultipartFile(
-                "file",
-                "image.jpg",
-                MediaType.IMAGE_JPEG_VALUE,
-                imageBytes
-        );
-        Adventure adventure = new Adventure("1", "name1", "quote1", "description1", "url1");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String adventureJson = objectMapper.writeValueAsString(adventure);
-        MockMultipartFile adventurePart = new MockMultipartFile(
-                "data",
-                "adventure.json",
-                MediaType.APPLICATION_JSON_VALUE,
-                adventureJson.getBytes()
-        );
-
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/adventures")
-                        .file(file)
-                        .file(adventurePart))
-                .andExpect(status().isOk())
-                .andExpect(content().json(
-                        """
-                                    {"data": {
-                                        "id": "1",
-                                        "quote": "quote1",
-                                        "name": "name1",
-                                        "description": "description1",
-                                        "url": "url1"
-                                    }
-                                    }
-                                """
-                ));
-    }
-
 
 }
 
