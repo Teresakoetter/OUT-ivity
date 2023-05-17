4ce30e4fb6922828d31c0133ec52e613ee0abd7b
teresakoetter <t.koetter@mailbox.org>
2023-05-17 14:13:25 +0200

    transportation added to frontend

--- frontend/src/AddAdventure.tsx
+++ frontend/src/AddAdventure.tsx
@@ -11,12 +11,13 @@ export default function AddAdventure(props: AddAdventureProps) {
     const [quote, setQuote] = useState("")
     const [description, setDescription] = useState("")
     const [image, setImage] = useState<File>()
+    const [transportation, setTransportation] = useState("")
     const navigate = useNavigate();
 
     function onSaveAdventure(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
-        const newAdventure: NewAdventure = {name: name, quote: quote, description: description}
+        const newAdventure: NewAdventure = {name: name, quote: quote, description: description, transportation: transportation}
 
         props.addAdventure(newAdventure, image)
         navigate("/adventures")
@@ -28,36 +29,55 @@ export default function AddAdventure(props: AddAdventureProps) {
             <form onSubmit={onSaveAdventure}>
                 <div className="textareaContainer">
                 <textarea className="name"
-                    placeholder="insert your adventure name"
-                    value={name}
-                    onChange={(event) => {
-                        setName(event.target.value)
-                    }}/>
-                <textarea
-                    className="quote"
-                    placeholder="share a quote with us"
-                    value={quote}
-                    onChange={(event) => {
-                        setQuote(event.target.value)
-                    }}/>
-                <textarea
-                    className="description"
-                    placeholder="we're excited to read the whole story"
-                    value={description}
-                    onChange={(event) => {
-                        setDescription(event.target.value)
-                    }}/>
-                <input type="file"
-                       placeholder="share a picture with us"
-                       onChange={(event) => {
-                    if (event.target.files) {
-                        setImage(event.target.files[0])
-                    }
-                }
-                }/>
+                          placeholder="insert your adventure name"
+                          value={name}
+                          onChange={(event) => {
+                              setName(event.target.value)
+                          }}/>
+                    <textarea
+                        className="quote"
+                        placeholder="share a quote with us"
+                        value={quote}
+                        onChange={(event) => {
+                            setQuote(event.target.value)
+                        }}/>
+                    <textarea
+                        className="description"
+                        placeholder="we're excited to read the whole story"
+                        value={description}
+                        onChange={(event) => {
+                            setDescription(event.target.value)
+                        }}/>
+                    <input type="file"
+                           className="imagefile"
+                           placeholder="share a picture with us"
+                           onChange={(event) => {
+                               if (event.target.files) {
+                                   setImage(event.target.files[0])
+                               }
+                           }
+                           }/>
+                    <label htmlFor="dropdown">
+                        <select
+                            className="dropdown"
+                            id="dropdown"
+                            name="dropdown"
+                            value={transportation}
+                            onChange={(event) => {
+                                setTransportation(event.target.value)
+                            }}>
+                            <option value="option1">choose your way of moving</option>
+                            <option value="option2">bicycle</option>
+                            <option value="option3">more wheels</option>
+                            <option value="option4">on the water</option>
+                            <option value="option5">through the air</option>
+                            <option value="option6">by foot</option>
+                        </select>
+                    </label>
                 </div>
+                <br/>
                 <button>save adventure</button>
-                </form>
+            </form>
         </div>
     )
 
--- frontend/src/Adventure.ts
+++ frontend/src/Adventure.ts
@@ -2,12 +2,14 @@ export type Adventure = {
     id: string,
     name: string,
     quote: string,
-    description: string
-    url: string
+    description: string,
+    url: string,
+    transportation: string
 }
 
 export type NewAdventure = {
     name: string,
     quote: string,
-    description: string
+    description: string,
+    transportation: string
 }
